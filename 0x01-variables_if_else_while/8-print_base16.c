2b913d93554e8c7d5f5ba5f32a9bfd0be04fc344
Sydon <syde_don@yahoo.com>
2022-09-15 02:29:53 -0700

    If variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,15 +13,15 @@ int n;
 for (n = 48; n <= 57; n++)
 {
 
-	putchar(n);
+putchar(n);
 }
 for (ch = 'a'; ch <= 'f'; ch++)
 {
 
-	putchar(ch);
+putchar(ch);
 }
 
-putchar(10)
+putchar(10);
 
-	return (0);
+return (0);
 }
